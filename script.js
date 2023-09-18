94bcc7417ded11e168e4351547a282536789a6b4
AbbeVass <134689455+AbbeVass@users.noreply.github.com>
2023-09-18 19:08:28 +0200

    simple update

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-fetch('yourfile.json')
-  .then(response => response.json())
-  .then(data => {
-    // Handle the JSON data here
-  })
-  .catch(error => console.error('Error fetching JSON:', error));
+let qData;
+let currentQuestion = 0;
+
+fetch('question-data.json')
+    .then(response => response.json())
+    .then(data => {
+        qData = data;
+        nextQuestion();
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
+
+function nextQuestion() {
+    if (currentQuestion < qData.length) {
+        currentQuestion++;
+
+        const progBar = document.getElementById("progressBar");
+        progBar.style.width = currentQuestion * (100/qData.length) + "%";
+        document.getElementById("progressText").innerHTML = currentQuestion+"/"+qData.length;
+
+    }
+    else {
+        alert("FÄRDIG") //End page
+    }
+}
+
