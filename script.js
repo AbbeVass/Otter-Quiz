57584226ce93eb5ef186d3e3aa2a6bb3ecf613ad
AbbeVass <134689455+AbbeVass@users.noreply.github.com>
2023-09-19 18:10:21 +0200

    Question page done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let qstData;
-let currentQuestion = 0;
+let currentQuestion = 3;
 let btnWorks = false;
+let choosenAnswer = -1;
+let points = 0;
 
 fetch('question-data.json')
     .then(response => response.json())
@@ -11,27 +13,51 @@ fetch('question-data.json')
     .catch(error => console.error('Error fetching JSON:', error));
 
 function nextQuestion() {
+    btnWorks = false;
+    document.getElementById("continueBtn").style.cursor = "default";
+
     if (currentQuestion < qstData.length) {
         currentQuestion++;
+        choosenAnswer = -1;
 
         const prgBar = document.getElementById("progressBar");
-        prgBar.style.width = currentQuestion * (100/qstData.length) + "%";
-        document.getElementById("progressText").innerHTML = currentQuestion+"/"+qstData.length;
+        prgBar.style.width = (currentQuestion * 100) / qstData.length + "%";
+        document.getElementById("progressText").innerHTML = currentQuestion + "/" + qstData.length;
 
-        const qst = qstData[currentQuestion-1];
+        const qst = qstData[currentQuestion - 1];
         document.getElementById("question").innerHTML = qst.question;
 
+        // Clear the answers container before adding new answers
+        const answersContainer = document.getElementById("answers");
+        answersContainer.innerHTML = "";
+
         for (let i = 0; i < qst.answers.length; i++) {
-            document.getElementById("answers").innerHTML += `
-                <div class="answer-container">
-                    <div class="checkmark"></div>
+            answersContainer.innerHTML += `
+                <div class="answer-container" id="answer${i}">
+                    <div class="checkmark" id="checkmark${i}"></div>
                     <p class="answer-text">${qst.answers[i]}</p>
                 </div>
             `;
         }
-    }
-    else {
-        alert("Färdig") //End page
+        for (let i = 0; i < qstData[currentQuestion - 1].answers.length; i++) {
+            document.getElementById("answer" + i).addEventListener("click", createClickListener(i));
+        }
+
+    } else {
+        document.getElementById("content").innerHTML = `
+            <h1>Du är inte värd ${points} uttrar!<h1>
+        `;
     }
 }
 
+function createClickListener(index) {
+    return function () {
+        for (checkmark of document.getElementsByClassName("checkmark")) {
+            checkmark.style.backgroundColor = "#000000";
+        }
+        document.getElementById("checkmark" + index).style.backgroundColor = "#aa82ff";
+        choosenAnswer = index;
+        btnWorks = true;
+        document.getElementById("continueBtn").style.cursor = "pointer";
+    };
+}
